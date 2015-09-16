5e21092130aff04a65421f17132a31679e5a27e3
shift <shiftcrurrency2@gmail.com>
2015-09-16 22:54:51 +0200

    progress

--- shiftex.py
+++ shiftex.py
@@ -16,6 +16,46 @@ def main():
     get_blocks()
 
 
+def clean_block(block_dict):
+
+    try:
+        c = block_dict['result'].copy()
+        del c['nonce']
+        del c['transactions']
+        del c['size']
+        del c['logsBloom']
+
+        c['gasLimit'] = int(c['gasLimit'], 16)
+        c['number'] = int(c['number'], 16)
+        c['difficulty'] = int(c['difficulty'], 16)
+        c['gasUsed'] = int(c['gasUsed'], 16)
+        c['totalDifficulty'] = int(c['totalDifficulty'], 16)
+
+    except Exception as e:
+        print "Could not alter keys in block dictionary: %s" % e
+
+    return c
+
+
+def clean_trans(trans_dict):
+
+    try:
+        c = trans_dict['result'].copy()
+        del c['input']
+        del c['nonce']
+
+        c['gas'] = int(c['gas'], 16)
+        c['value'] = int(c['value'], 16)
+        c['blockNumber'] = int(c['blockNumber'], 16)
+        c['transactionIndex'] = int(c['transactionIndex'], 16)
+        c['gasPrice'] = int(c['gasPrice'], 16)
+
+    except Exception as e:
+        print "Could not alter keys in transaction dictionary: %s" % e
+
+    return c
+
+
 def db_insert(blocks, transactions):
     
     try:
@@ -24,9 +64,13 @@ def db_insert(blocks, transactions):
     except Exception as e:
         print "Could not connect to database: %s" % e
 
+    if (config.get("general", "debug")) == "on":
+        print "Inserting %d blocks and %d transactions into database" % (len(blocks),len(transactions))
+
     try:
+        print blocks
         c.executemany('INSERT INTO blocks VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)', blocks)
-        c.executemany('INSERT INTO transactions VALUES (?,?,?,?,?,?,?,?,?,?)', transaction)
+        c.executemany('INSERT INTO transactions VALUES (?,?,?,?,?,?,?,?,?,?)', transactions)
     except Exception as e:
         print "Could not insert values into database: %s. Exiting." % e
         sys.exit(0)
@@ -71,22 +115,27 @@ def get_blocks():
 
 	block_dict = json.loads(res.content)
 
-            blocks.append(tuple([v for v in block_dict['result'].values()]))    
+        if block_dict['result']:
+            c = clean_block(block_dict)
+            blocks.append(tuple([v for v in c.values()])) 
+        
             if len(block_dict['result']['transactions']) > 0:
                 trans_dict = get_transactions(block_dict['result']['transactions'])
-                transactions.append(tuple([v for v in trans_dict['result'].values()]))
-            
-            if (block_counter % int(config.get("database", "insert_limit"))) == 0:
-                res = db_insert(blocks, transactions)
-                blocks = []
-                transactions = []
+                c = clean_trans(trans_dict) 
+                transactions.append(tuple([v for v in c.values()]))
+
+            block_counter+=1
         
         else:
             if (config.get("general", "debug")) == "on":
-                print "Reached a none existing block (%d). Exiting." % block_counter
+                print "Reached a none existing block (%d), end of blockchain?. Exiting." % block_counter
                 return False
 
-        block_counter+=1
+        if (block_counter % int(config.get("database", "num_inserts"))) == 0:
+            res = db_insert(blocks, transactions)
+            blocks = []
+            transactions = []
+
 
 
 if __name__ == "__main__":
